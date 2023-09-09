e471e573e6b0978d67f2eeb33025d40330a22bef
Rafa Garcia Rea <rafagarciarea@gmail.com>
2023-09-09 08:39:40 +0200

    Crear theme cliniccaremadrid

---
+++ docroot/themes/custom/cliniccaremadrid/js/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Behaviors for the cliniccaremadrid theme.
+ */
+
+(function ($, _, Drupal) {
+  Drupal.behaviors.cliniccaremadrid = {
+    attach() {
+      // cliniccaremadrid JavaScript behaviors goes here.
+    },
+  };
+})(window.jQuery, window._, window.Drupal);
